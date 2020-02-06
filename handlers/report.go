13aeb0b866afee2727e5bbd3333519ec5d56b025
toby <twabias@gmail.com>
2020-02-06 09:32:05 +0100

    adding env var to markdown generator

--- handlers/report.go
+++ handlers/report.go
@@ -4,12 +4,20 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-
+	"os"
 	"flag"
 	"html/template"
 )
 
-var domain = flag.String("domain", "goreportcard.com", "Domain used for your goreportcard installation")
+func env(key string, defaultKey string) string {
+    value, exists := os.LookupEnv(key)
+    if !exists {
+        value = defaultKey
+    }
+    return value
+}
+
+var domain = flag.String("domain", env("DOMAIN", "goreportcard.com"), "Domain used for your goreportcard installation")
 var googleAnalyticsKey = flag.String("google_analytics_key", "UA-58936835-1", "Google Analytics Account Id")
 
 // ReportHandler handles the report page
